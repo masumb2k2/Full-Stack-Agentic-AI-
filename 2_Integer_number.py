613edad4c108714204c33c584dc5d6450ef36fc3
MASUM BILLAH <masumb2k2@gmail.com>
2026-01-30 23:58:25 +0600

    Integer Data types

---
+++ 2_Integer_number.py
@@ -0,0 +1,11 @@
+shop_item_amount = 150
+new_added_amount = 10
+
+total_shop_item_amount = shop_item_amount + new_added_amount
+print(f"After Add Total Shop item Amount: {total_shop_item_amount}")
+
+stock_out_item_amount=12
+after_stock_out_shot_item_amount = total_shop_item_amount - stock_out_item_amount
+print(f"After stock out Total Shop item Amount: {after_stock_out_shot_item_amount}")
+
+# * / ** % 
